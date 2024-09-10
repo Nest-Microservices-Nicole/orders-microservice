8b872c3672db0f180940f9eca24a2609a2c2dd94
nicobv01 <nicolbatista011@gmail.com>
2024-09-10 16:24:01 -0400

    detalle de orden

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -118,7 +118,16 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   async findOne(id: string) {
 
     const order = await this.order.findFirst({
-      where: { id }
+      where: { id },
+      include: {
+        OrderItem: {
+          select: {
+            price: true,
+            quantity: true,
+            productId: true
+          }
+        }
+      }
     });
 
     if ( !order ) {
@@ -128,7 +137,19 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
       });
     }
 
-    return order;
+    const productIds = order.OrderItem.map( orderItem => orderItem.productId );
+
+    const products = await firstValueFrom(
+      this.productsClient.send({ cmd: 'validate_products' }, productIds)
+    );
+
+    return {
+      ...order,
+      OrderItem: order.OrderItem.map( orderItem => ({
+        ...orderItem,
+        product: products.find( product => product.id === orderItem.productId ).name
+      }))
+    };
 
   }
 
